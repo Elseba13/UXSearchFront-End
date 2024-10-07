c71c4f9886dea648c9c2a18d6956b6570e3af810
unknown <sebastian.quiroga.v@mail.pucv.cl>
2024-10-07 11:42:04 -0300

    implementación barra de navegación

---
+++ uxsearch/src/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-dark bg-primary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">UXSearch</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                 </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-link" href="#">Metodos de evaluación UX</a>
+                        <a className="nav-link" href="#">Iniciar Sesión como administrador</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    ); 
+}
+
+export default Navbar; 
