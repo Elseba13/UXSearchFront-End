247778b6ad7f2d397f389b41151fea689f8b97e2
Elseba13 <114709397+Elseba13@users.noreply.github.com>
2024-10-22 20:19:44 -0300

    diseño del home

--- uxsearch/src/Header.js
+++ uxsearch/src/Header.js
@@ -1,26 +1,39 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './styles/Navbar.css'; 
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-dark bg-primary">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">UXSearch</a>
+            
+            
+                <a className="navbar-brand" href="/">
+                    UXSearch
+                </a>
+
+            
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
-                 </button>
+                </button>
+
+            
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
+                    <div className="navbar-nav mx-auto">
                         <a className="nav-link text-white" href="/pantalla-principal">Listado métodos</a>
                         <a className="nav-link text-white" href="/explicacion-filtros">Explicación filtros</a>
-                        <a className="nav-link text-white" href="/dashboard">Métodos disponibles por filtros</a>
-                        <a className="nav-link text-white" href="/login-admin">Iniciar Sesión como administrador</a>                
+                        <a className="nav-link text-white" href="/dashboard">Métodos disponibles por filtros</a>              
+                    </div>
+
+                
+                    <div className="d-flex">
+                        <a href="/login-admin" className="btn btn-outline-light ms-lg-2">Login</a>
                     </div>
                 </div>
             </div>
         </nav>
-    ); 
+    );
 }
 
-export default Navbar; 
+export default Navbar;
--- uxsearch/src/Home.js
+++ uxsearch/src/Home.js
@@ -3,56 +3,60 @@ import { useNavigate } from "react-router-dom";
 import Navbar from "./Header";
 
 const Home = () => {
-    
-    const navigate = useNavigate(); 
-
-    const handleStart = () => {
-        navigate('/pantalla-principal'); 
-    }; 
 
     return(
         <>
-            <Navbar/>
-            <main className='d-flex flex-column justify-content-center align-items-center text-center'
-            style={{
-                backgroundImage: 'url("https://www.itdo.com/blog/content/images/2020/12/ITDO-diseno.jpg")',
-                backgroundSize: 'cover', 
-                backgroundPosition: 'center', 
-                backgroundAttachment: 'fixed',
-                height: 'calc(100vh - 56px)', 
-                color: 'white',
-             }}
-            > 
-
-            <div 
+           <Navbar /> 
+           <section 
+                className='d-flex flex-column justify-content-center align-items-start text-start'
                 style={{
-                    width: '80%',
-                    height: '40%',
-                    backgroundColor:'white',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    borderRadius: '20px',
-                    overflow: 'hidden',
-                    textAlign: 'center',
-                    marginBottom: '5%'
+                    backgroundImage: 'linear-gradient(135deg, #00FF7F 0%, #00BFFF 50%, #FFFFFF 100%)',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundAttachment: 'fixed',
+                    height: '70vh', 
+                    color: 'white',
+                    padding: '2rem' // Ajusta el padding según lo que necesites
                 }}
             >
-            <p style={{fontSize: '1.2rem', margin: '10%', flexGrow: 1, color: 'black', textAlign: 'center'}}>
-                En UX Search podras encontrar información acerca de diferentes métodos de evaluación UX, en conjunto a sus fuentes de origen, y un apartado para la selección de filtros de búsqueda los cuales permitirán encontrar el método acertado para tu proyecto.
-            </p>
-        </div>
+                <h1 style={{ fontSize: '3rem', fontWeight: 'bold', color: '#2d2d2d' }}>
+                    Bienvenido a <span style={{ color: '#0D6EFD' }}>UXSearch</span>
+                </h1>
+                <p style={{ fontSize: '1.25rem', marginTop: '1rem', color: '#2d2d2d' }}>
+                    Una plataforma donde podrás encontrar una gran cantidad de métodos de evaluación UX.
+                </p>
+               
+            </section>
 
-        <div> 
-            <button 
-                type='button' 
-                className='btn btn-primary btn-lg' 
-                onClick={handleStart}  
-        >
-            Comenzar
-            </button>
-        </div>
-        </main>
+
+            <h2 
+                style={{
+                    fontSize: '2rem', 
+                    fontWeight: 'bold', 
+                    color: "#0D6EFD", 
+                    margin: '3rem 2rem 2rem auto', // Ajustar márgenes: top, right, bottom, left
+                    textAlign: 'left', // Alinear el texto a la derecha
+                    width: '90%' // Darle ancho para que se desplace a la derecha
+                }}
+            >
+                Introduciendo a UXSearch
+            </h2>
+
+            <section className="d-flex flex-row justify-content-around" style={{padding: '3rem 2rem', backgroundColor: '#ff5f5f5'}}>
+
+
+                <div style={{width: '20%', textAlign: 'center'}}>
+                    <img src="https://cdn-icons-png.flaticon.com/512/13191/13191289.png" alt="Imagen 1" style={{width: '100%'}}/>
+                    <h3 style={{marginTop: '1rem'}}>Acerca de nosotros</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+
+                <div style={{width: '20%', textAlign: 'center'}}>
+                    <img src="https://cdn-icons-png.flaticon.com/512/12421/12421344.png" alt="Imagen 2" style={{width: '100%'}}/>
+                    <h3 style={{marginTop: '1rem'}}>¿Cual es nuestro objetivo?</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>                   
+                </div>
+            </section>
         </>
         
         
